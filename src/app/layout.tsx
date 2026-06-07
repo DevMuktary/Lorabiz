6662fe324c771146b8deec961aecf3d609c4a3ff
MUKTAR ABDULWAHEED <nou261105134@noun.edu.ng>
2026-06-07 23:45:19 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-50 overscroll-none" suppressHydrationWarning>
-      <body className={`${jetbrainsMono.className} ${jetbrainsMono.variable} antialiased bg-slate-50 text-slate-900 min-h-screen flex flex-col overscroll-none`}>
+    <html lang="en" suppressHydrationWarning>
+      {/* 
+        Changed min-h-screen to min-h-[100dvh] to fix the Safari toolbar bug
+        Removed overscroll-none to allow the toolbar to collapse on scroll
+      */}
+      <body className={`${jetbrainsMono.className} ${jetbrainsMono.variable} antialiased bg-slate-50 text-slate-900 min-h-[100dvh] flex flex-col`}>
         <Providers>
           {children}
         </Providers>
