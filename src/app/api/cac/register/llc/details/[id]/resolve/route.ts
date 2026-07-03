ef6fef280c54cc5dfe5fad87c4e279343039850c
MUKTAR ABDULWAHEED <nou261105134@noun.edu.ng>
2026-07-03 04:56:19 +0100

    Fix import statement for prisma in route.ts

--- src/app/api/cac/register/llc/details/[id]/resolve/route.ts
+++ src/app/api/cac/register/llc/details/[id]/resolve/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma"; // Adjust import to match your database client
+import { prisma } from "@/lib/prisma"; // Adjust import to match your database client
 
 export async function POST(
   req: NextRequest,
