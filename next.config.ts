61da18520667a0190e0f7b966c52a0a63f34ab18
MUKTAR ABDULWAHEED <nou261105134@noun.edu.ng>
2026-07-25 11:41:51 +0100

    Remove instrumentation hook and update comments

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 👉 ADD THIS: This is required to make Next.js run your instrumentation.ts file!
-  experimental: {
-    instrumentationHook: true,
-  },
+  // Leave this empty again! instrumentation.ts runs automatically now.
 };
 
 export default withSentryConfig(nextConfig, {
@@ -26,21 +23,14 @@ export default withSentryConfig(nextConfig, {
   widenClientFileUpload: true,
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: "/monitoring",
 
   webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
+    // Enables automatic instrumentation of Vercel Cron Monitors.
     automaticVercelMonitors: true,
 
     // Tree-shaking options for reducing bundle size
     treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
       removeDebugLogging: true,
     },
   }
