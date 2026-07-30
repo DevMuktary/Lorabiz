ad5a4bb82e6088d2f866509519d086bb6a8665d2
MUKTAR ABDULWAHEED <nou261105134@noun.edu.ng>
2026-07-30 19:43:03 +0100

    Refactor next.config.ts for PWA and Sentry integration
    Refactor next.config.ts to use ES module syntax and integrate Sentry with PWA configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,14 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
+import { withSentryConfig } from "@sentry/nextjs";
+import withPWAInit from "@ducanh2912/next-pwa";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Leave this empty again! instrumentation.ts runs automatically now.
+  reactStrictMode: true,
+};
+
+// 1. Initialize PWA Configuration
+const withPWA = withPWAInit({
   dest: "public",
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
@@ -10,10 +20,36 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   },
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Your existing Next.js config rules go here
-  reactStrictMode: true,
-};
+// 2. Wrap NextConfig with PWA
+const pwaConfig = withPWA(nextConfig);
 
-module.exports = withPWA(nextConfig);
+// 3. Wrap the resulting PWA Config with Sentry
+export default withSentryConfig(pwaConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: "quadrox-technologies-limited",
+  project: "javascript-nextjs",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  tunnelRoute: "/monitoring",
+
+  webpack: {
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    automaticVercelMonitors: true,
+
+    // Tree-shaking options for reducing bundle size
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  }
+});
