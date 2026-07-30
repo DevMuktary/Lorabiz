ec22427facb1ba270383453e00c41c21580c1dca
MUKTAR ABDULWAHEED <nou261105134@noun.edu.ng>
2026-07-30 19:38:38 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,19 @@
-import { withSentryConfig } from "@sentry/nextjs";
-import type { NextConfig } from "next";
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
-const nextConfig: NextConfig = {
-  // Leave this empty again! instrumentation.ts runs automatically now.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing Next.js config rules go here
+  reactStrictMode: true,
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: "quadrox-technologies-limited",
-
-  project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  tunnelRoute: "/monitoring",
-
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      removeDebugLogging: true,
-    },
-  }
-});
+module.exports = withPWA(nextConfig);
