401b67607e1f35b9376282051f52489b7e789c04
MUKTAR ABDULWAHEED <nou261105134@noun.edu.ng>
2026-07-30 20:33:43 +0100

    Add reference for web worker in sw.ts

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,4 +1,6 @@
 // src/app/sw.ts
+/// <reference lib="webworker" />
+
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
